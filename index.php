06e3cccf0f7cf5656ee22831d236f789f9e12f28
Steven Woolston <steven.woolston@gmail.com>
2019-10-30 17:05:57 +1000

    WIP mailer

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 include_once 'api/autoload.php';
 use config\Database;
+use config\Mailer;
 use v1\objects\Website;
 
 $database = new Database();
 $db = $database->getConnection();
 $website = new Website($db);
+$mailer = new Mailer();
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -22,20 +24,24 @@ $err = curl_error($curl);
 curl_close($curl);
 $data = json_decode($response, true);
 $sites = $data["data"];
+$html_body = null;
 
 foreach($sites as $site) {
     $url = $site['URL'];
     if ($url) {
         date_default_timezone_set('Australia/Melbourne');
         $startTime = new \DateTime();
-        $headers = get_headers($url, 1);
+        // $headers = get_headers($url, 1);
         $endTime = new \DateTime();
 
         $website->SiteUrl = $url;
         $website->MonitorHttpCode = $httpcode;
         $website->MonitorResponseBody = json_encode($headers);
         $response = $website->create();
-        echo "<h1>" .$url. "<span style='font-size: 0.8em'>" .$httpcode. "</span></h1>";
-        echo "<hr />";
+        $html_body .= "<h1>" .$url. "<span style='font-size: 0.8em'>" .$httpcode. "</span></h1>";
+        $html_body .= "<hr />";
     }
 }
+
+$mailer->email_body = $html_body;
+$mailer->send_email();
